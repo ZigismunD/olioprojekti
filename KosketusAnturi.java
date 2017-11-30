9e48a2da1d47ecaebdcc530b659f5265d3dcc539
topiasre <34054306+topiasre@users.noreply.github.com>
2017-11-30 09:40:19 +0200

    Update KosketusAnturi.java

--- KosketusAnturi.java
+++ KosketusAnturi.java
@@ -1,5 +1,27 @@
 package olioprojekti;
 
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.Button;
+
 public class KosketusAnturi {
 
+	private EV3TouchSensor Kytkin;
+	float[] sample = new float[Kytkin.sampleSize()];
+	
+	public KosketusAnturi(EV3TouchSensor ts) {
+		this.Kytkin = ts;
+	}
+	
+	public void Lukko() {
+		while(true) {
+			Button.LEDPattern(2);
+			Kytkin.fetchSample(sample, 0);
+			if(sample[0] == 1) {
+				Button.LEDPattern(1);
+				break;
+			} else {
+				continue;
+			}
+		}
+	}
 }
