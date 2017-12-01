d73bfdde4300a3369857da69e147cc19f9115898
Fleesi <34022880+Fleesi@users.noreply.github.com>
2017-12-01 12:53:58 +0200

    Add files via upload

---
+++ Aani.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+import lejos.hardware.Sound;
+
+public class Aani {
+	
+	static File musiikki = new File("polkka2.wav");
+	static File auto = new File("autostart.wav");
+
+	public static void kaynnista() {
+		Sound.playSample(auto);
+	}
+	public static void kappale() {
+		Sound.playSample(musiikki);
+	}
+}
