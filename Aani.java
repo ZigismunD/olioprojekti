ace6c69501fc676d460d28b8cb2f6f2a46895e61
Sami <zigismund90@gmail.com>
2017-12-11 09:33:22 +0200

    Javadoc

--- Aani.java
+++ Aani.java
@@ -2,20 +2,33 @@ package olioprojekti;
 
 import java.io.File;
 
-import lejos.hardware.Sound;
 
+
+import lejos.hardware.Sound;
+/**
+ * Aani class allows the program to play sounds.
+ * Aani runs in a thread so program can still take commands from remote control.
+ * @author zigi
+ *
+ */
 public class Aani extends Thread {
 	private boolean running = true;
 	private int efekti;
 	
 	private File musiikki = new File("polkka2.wav");
 	private File auto = new File("autostart.wav");
-	
+	/**
+	 * Consturctor for the class Aani.
+	 * Sets variable efekti to 0.
+	 */
 	public Aani() {
 		this.efekti = 0;
 	}
 
-	
+	/**
+	 * Checks the variable efekti.
+	 * if efekti is something else than 0, plays the sound with that number.
+	 */
 	public void run() {
 		while (running) {
 			switch (efekti) {
@@ -30,18 +43,28 @@ public class Aani extends Thread {
 			}
 		}
 	}
-	
+	/**
+	 * Alters the state of boolean 'running' to false.
+	 */
 	public void running() {
 		this.running = false;
 	}
-
+	/**
+	 * Plays the sound of car starting.
+	 */
 	public void kaynnista() {
 		Sound.playSample(auto);
 	}
+	/**
+	 * Plays a short version of the son Ievan Polkka.
+	 */
 	public void kappale() {
 		Sound.playSample(musiikki);
 	}
-	
+	/**
+	 * Alters the variable efekti to allow sounds to be played.
+	 * @param aani The number of the desired sound effect.
+	 */
 	public void efekti(int aani) {
 		this.efekti = aani;
 	}
